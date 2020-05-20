148a62495d9dd72f306bfaddab059688a04b3aef
Greg Pasquariello <greg@pasq.net>
2020-05-19 23:04:10 -0500

    Set up a demo SendGrid account.  You can use this for testing.

--- test.ts
+++ test.ts
@@ -11,26 +11,30 @@ import {
   ISimpleRequestBody,
 } from "./mod.ts";
 
+// This is a throwaway key for testing a limited number of emails only,
+// from a throwaway account.  Sorta like giving you my keys, but the keys
+// are to an outhouse.
+const key =
+  "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";
+
 Deno.test({
   name: "Testing Simple Email",
   async fn(): Promise<void> {
     let response = await sendSimpleMail(
       {
         subject: "Hello world",
-        to: [{ email: "foo@bar.com" }],
-        from: { email: "from@bar.com" },
+        to: [{ email: "denotest2020@gmail.com" }],
+        from: { email: "denotest2020@gmail.com" },
         content: [
           { type: "text/plain", value: "Hello world" },
           { type: "text/html", value: "<h1>Hello world</h1>" },
         ],
       },
-      {
-        apiKey: "REDACTED",
-      },
+      { apiKey: key },
     );
 
     console.log(response);
-    assertEquals(response.success, false);
+    assertEquals(response.success, true);
   },
 });
 
@@ -41,19 +45,19 @@ Deno.test({
       personalizations: [
         {
           subject: "Hello world",
-          to: [{ name: "Greg Pasquariello", email: "greg@pasq.net" }],
+          to: [{ name: "DenoTest2020", email: "denotest2020@gmail.com" }],
         },
       ],
-      from: { email: "greg@pasq.net" },
+      from: { email: "denotest2020@gmail.com" },
       content: [
         { type: "text/plain", value: "Hello world" },
         { type: "text/html", value: "<h1>Hello world</h1>" },
       ],
     };
 
-    let response = await sendMail(mail, { apiKey: "XXX" });
+    let response = await sendMail(mail, { apiKey: key });
 
     console.log(response);
-    assertEquals(response.success, false);
+    assertEquals(response.success, true);
   },
 });
